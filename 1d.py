ec4855299bb230dd2348811c32ed0a038df3c55a
Ammar2380 <ammar211132@gmail.com>
2026-02-05 12:57:38 +0500

    new file:   1d.py 	new file:   2d.py 	new file:   arr.py 	modified:   main.py

---
+++ 1d.py
@@ -0,0 +1,6 @@
+# one dimensional array
+
+marks = [22,22,44,66,44,21,12]
+import numpy as np
+arr1_1d = np.array([10,20,30,40,50])
+print(arr1_1d)
---
+++ main.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+python_list = [1,2,3,4]
+print(python_list)
